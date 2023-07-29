e4d0b57ff10bb00896a6d4f4790dba20ab8a9676
Blacktuna1 <93248801+Blacktuna1@users.noreply.github.com>
2023-07-29 20:51:48 +0800

    加几个模块

---
+++ ken-blog/src/main/java/com/kenblog/KenBlogApplication.java
@@ -0,0 +1,11 @@
+package com.kenblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KenBlogApplication.class,args);
+    }
+}
