b43e220c95225c59273e8d369320a39be2352609
Blacktuna1 <93248801+Blacktuna1@users.noreply.github.com>
2023-08-21 16:53:31 +0800

    给导出excel加个权限控制

--- ken-admin/src/main/java/com/kenblog/controller/CategoryController.java
+++ ken-admin/src/main/java/com/kenblog/controller/CategoryController.java
@@ -1,13 +1,23 @@
 package com.kenblog.controller;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.fastjson.JSON;
 import com.kenblog.ken.config.ResponseResult;
+import com.kenblog.ken.domain.entity.Category;
+import com.kenblog.ken.domain.vo.ExcelCategoryVo;
+import com.kenblog.ken.enums.AppHttpCodeEnum;
 import com.kenblog.ken.service.CategoryService;
+import com.kenblog.ken.utils.BeanCopyUtils;
+import com.kenblog.ken.utils.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @RestController
 @RequestMapping("/content/category")
@@ -20,5 +30,26 @@ public class CategoryController {
         return categoryService.getCategoryListAdmin();
     }
 
-    @GetMapping("export")
-}
+    @PreAuthorize("@ps.hasPermission('content:category:export')")
+    @GetMapping("/export")
+    public void export(HttpServletResponse response){
+        try {
+            //设置下载文件的请求头
+            WebUtils.setDownLoadHeader("分类.xlsx",response);
+            //获取需要导出的数据
+            List<Category> categoryVos = categoryService.list();
+
+            List<ExcelCategoryVo> excelCategoryVos = BeanCopyUtils.copyBeanList(categoryVos, ExcelCategoryVo.class);
+            //把数据写入到Excel中
+            EasyExcel
+                    .write(response.getOutputStream(), ExcelCategoryVo.class)
+                    .autoCloseStream(Boolean.FALSE)
+                    .sheet("分类导出")
+                    .doWrite(excelCategoryVos);
+
+        } catch (Exception e) {
+            //如果出现异常也要响应json
+            ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR);
+            WebUtils.renderString(response, JSON.toJSONString(result));
+        }
+    }}
